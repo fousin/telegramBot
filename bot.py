73a1dc4c988e59aef52b6a44a51df93925540d2e
fousin <79547236+fousin@users.noreply.github.com>
2022-10-23 00:16:04 -0300

    A1

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-from email import message
-from turtle import update
-from telegram.ext import Updater,CommandHandler
+from telegram.ext import (Updater, CommandHandler)
+
 
 def welcome(update, context):
     message = 'Olá, ' + update.message.from_user.first_name + '!'
@@ -15,13 +14,14 @@ def main():
     token = '5748916825:AAHlmxwXRFdwCkRBnJOW7C4HMuqt9pnKXYs'
     updater = Updater(token=token, use_context=True)
 
-    updater.dispatcher.add_handler(CommandHandler('start',welcome))#cria comando start e chama metodo welcome
+    updater.dispatcher.add_handler(CommandHandler('start', welcome))#cria comando start e chama metodo welcome
     
 
-    updater.start_polling()
+    
     print('oi, eu sou o updater ' + str(updater))
+
+    updater.start_polling()
     updater.idle()
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
-
