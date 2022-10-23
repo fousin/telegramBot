1fdb0b34a48f50c662fb33330e194a1a8e112bb5
fousin <79547236+fousin@users.noreply.github.com>
2022-10-23 03:45:32 -0300

    teste

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def inputFaq1(update, context):
     
 
 def inputFim(update, context):
-    #feedback = update.message.text
+    
     message = "em breve um bot mais complexo"
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
